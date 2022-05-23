bb5c26ab225d32b392ea8411fa93a36e5373f256
VicMaster289 <vcejam1600@alumno.ipn.mx>
2022-05-23 18:14:01 -0500

    conexion servidor bd

--- php/conexionbd.php
+++ php/conexionbd.php
@@ -1,13 +1,16 @@
 <?php
-    $host="localhost";
+    /*$host="csdbserver.mysql.database.azure.com";
     $port=3306;
     $socket="";
-    $user="root";
-    $password="n0m3l0";
-    $dbname="COMSAFE";
+    $user="top";
+    $password="AZCSDBpass1";
+    $dbname="comsafe";
     
     $con = new mysqli($host, $user, $password, $dbname, $port, $socket)
         or die ('Could not connect to the database server' . mysqli_connect_error());
-    
+    */
+    $conn = mysqli_init(); 
+    mysqli_ssl_set($conn,NULL,NULL, "D:\DigiCertGlobalRootCA.crt.pem", NULL, NULL); 
+    mysqli_real_connect($conn, "csdbserver.mysql.database.azure.com", "top", "AZCSDBpass1", "comsafe", 3306, MYSQLI_CLIENT_SSL);
     //$con->close();
     ?>
